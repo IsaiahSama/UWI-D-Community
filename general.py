56ab84d964edc83d21bbb7d9888271a00873a5d9
Isaiah-Sama <zeldaplayergl@gmail.com>
2021-06-04 22:06:23 -0400

    Added in voice state control

--- general.py
+++ general.py
@@ -1,7 +1,43 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
+from botdata import channels, vc_to_tc
+from discord import PermissionOverwrite
 
-class EventHandler(commands.Cog):
-    pass 
+class General(commands.Cog):
+
+    """ Class with general commands available to all members """
+    
+    def __init__(self, bot) -> None:
+        self.bot = bot
+        bot.loop.create_task(self.async_init())
+
+    in_vc = []
+
+    @commands.command(brief="Creates a temporary text channel", help="Creates a temporary text channel to be used while in a voice channel")
+    async def speakme(self, ctx):
+        if not ctx.author.voice.channel:
+            await ctx.send("You are not in a voice channel")
+            return 
+
+        channel = ctx.guild.get_channel(vc_to_tc[ctx.author.voice.channel.id])
+
+        overwrites = channel.overwrites
+
+        for member in ctx.author.voice.channel.members:
+            overwrites[member] = PermissionOverwrite(view_channel=True)
+
+        await channel.edit(overwrites=overwrites)
+        await ctx.send(f"Press here to join the Text Channel for your Voice channel: {channel.mention}")
+        self.in_vc.append(ctx.author.id)
+
+    # Listener for VC change
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        if after.channel is None and before.channel is not None and member.id in self.in_vc:
+            overwrites = before.channel.overwrites
+            del overwrites[member]            
+            await before.channel.edit(overwrites=overwrites)
+
 
 def setup(bot):
-    bot.add_cog(EventHandler(bot))
+    bot.add_cog(General(bot))
